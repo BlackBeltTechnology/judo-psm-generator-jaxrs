e894485ecee698b46f11fe48406820a93b6cb818
Madmat8 <Mateka6400@gmail.com>
2023-12-22 16:36:07 +0100

    JNG-5342 generate rest api doc

---
+++ judo-psm-generator-jaxrs-api/src/main/java/hu/blackbelt/judo/psm/generator/jackson/api/RelationHelper.java
@@ -0,0 +1,10 @@
+package hu.blackbelt.judo.psm.generator.jaxrs.api;
+
+import hu.blackbelt.judo.meta.psm.derived.ReferenceAccessor;
+import hu.blackbelt.judo.meta.psm.service.TransferObjectRelation;
+
+public class RelationHelper {
+
+
+
+}
